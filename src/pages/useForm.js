6d7253e7d35199fccba2af5575768d961d88591b
Pong <silapong2000@gmail.com>
2023-05-05 01:30:32 +0700

    fifth

--- src/pages/useForm.js
+++ src/pages/useForm.js
@@ -5,44 +5,44 @@ import { useHistory } from "react-router-dom";
 import { AuthContext } from '../context/AuthContext';
 
 
-const useForm = (callback, validate) => {
-  const [values, setValues] = useState({
-    username: '',
-    email: '',
-    password: '',
-    password2: ''
-  });
-  const [errors, setErrors] = useState({});
-  const [isSubmitting, setIsSubmitting] = useState(false);
+// const useForm = (callback, validate) => {
+//   const [values, setValues] = useState({
+//     username: '',
+//     email: '',
+//     password: '',
+//     password2: ''
+//   });
+//   const [errors, setErrors] = useState({});
+//   const [isSubmitting, setIsSubmitting] = useState(false);
 
-  const handleChange = e => {
-    const { name, value } = e.target;
-    setValues({
-      ...values,
-      [name]: value
-    });
-  };
+//   const handleChange = e => {
+//     const { name, value } = e.target;
+//     setValues({
+//       ...values,
+//       [name]: value
+//     });
+//   };
 
-  const handleSubmit = e => {
-    e.preventDefault();
+//   const handleSubmit = e => {
+//     e.preventDefault();
 
-    setErrors(validate(values));
-    setIsSubmitting(true);
-  };
+//     setErrors(validate(values));
+//     setIsSubmitting(true);
+//   };
 
-  useEffect(
-    () => {
-      if (Object.keys(errors).length === 0 && isSubmitting) {
-        callback();
-      }
-    },
-    [errors]
-  );
+//   useEffect(
+//     () => {
+//       if (Object.keys(errors).length === 0 && isSubmitting) {
+//         callback();
+//       }
+//     },
+//     [errors]
+//   );
 
-  return { handleChange, handleSubmit, values, errors };
-};
+//   return { handleChange, handleSubmit, values, errors };
+// };
 
-export default useForm;
+// export default useForm;
 
 export const useLogin = () => {
     const [error, setError] = useState(false); 
